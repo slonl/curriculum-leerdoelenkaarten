9c788f6335a38d9228b126dd917b1916c9055da6
Alexander Kacharava <alexander@muze.nl>
2022-08-25 10:38:44 +0200

    Remove unused variables

--- test/test.mjs
+++ test/test.mjs
@@ -3,15 +3,15 @@ import Curriculum from "curriculum-js";
 async function validate() {
 	var curriculum = new Curriculum();
 	var schema = await curriculum.loadContextFromFile(
-		"ldk-schema",
-		"context.json"
-	);
-	var examenSchema = await curriculum.loadContextFromFile(
+    "curriculum-leerdoelenkaarten",
+    "context.json"
+  );
+	await curriculum.loadContextFromFile(
 		"curriculum-basis",
 		"curriculum-basis/context.json"
 	);
 	try {
-		let result = await curriculum.validate(schema);
+		await curriculum.validate(schema);
 		console.log("Data is valid!");
 	} catch (error) {
 		error.validationErrors.forEach((error) => {
